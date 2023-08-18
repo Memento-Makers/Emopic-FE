6f6c0562f3ceef15ceabff4c749ead5b1da39114
dar-jeeling <o_o___t@pusan.ac.kr>
2023-08-19 01:07:47 +0900

    ✨ feat: 검색 페이지에 FloatingButton 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
-import { BasicHeader } from '@/components';
+import { BasicHeader, FloatingButton } from '@/components';
 
 export default function Home() {
   return (
-    <>
+    <div className="h-[100vh] relative">
       {/* TODO: 더미 이미지 사용, 백엔드에게 더미 유저 프로필 사진 내려달라고 하기 */}
       <BasicHeader profileImage="https://picsum.photos/200" />
-      <main data-theme="retro" className="bg-black h-[100vh]">
+      <main className="bg-black ">
         검색
+        <FloatingButton />
       </main>
-    </>
+    </div>
   );
 }
