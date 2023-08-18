93026a8aa9a711a91b61802440b3f409993eebc7
dar-jeeling <o_o___t@pusan.ac.kr>
2023-08-19 01:07:19 +0900

    🤡 mock: 임시 이미지 업로드 페이지 파일 생성

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import { BasicHeader } from '@/components';
+
+export default function Home() {
+  return (
+    <>
+      {/* TODO: 더미 이미지 사용, 백엔드에게 더미 유저 프로필 사진 내려달라고 하기 */}
+      <BasicHeader profileImage="https://picsum.photos/200" />
+      <main data-theme="retro" className="bg-black h-[100vh]">
+        <p className=" text-[48px] text-white font-semibold">이미지 업로드</p>
+      </main>
+    </>
+  );
+}
