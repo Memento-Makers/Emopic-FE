ad653c4e76f04f40db96338dbb543e19aa28b7b2
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-08-23 12:52:56 +0900

    ✨ feat: FloatingButton 컴포넌트 구현 (#23)

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,6 @@ export * from './BasicHeader';
 export * from './Avatar';
 export * from './StyledImage';
 export * from './Logo';
+export * from './FloatingButton';
 export * from './PreviewList';
 export * from './Spacer';
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
-import { BasicHeader, PreviewList, Spacer } from '@/components';
+import Image from 'next/image';
+import { BasicHeader, PreviewList, Spacer, FloatingButton } from '@/components';
 
-// TODO: 모킹한 데이터로 수정
 
 // 분류 별 더미 데이터 생성 함수
 function createDummy() {
@@ -19,10 +19,10 @@ const categories = [
 
 export default function Home() {
   return (
-    <>
-      {/* TODO: 더미 이미지 사용, 백엔드에게 더미 유저 프로필 사진 내려달라고 하기 */}
+    <div className="h-[100vh] relative">
       <BasicHeader profileImage="https://picsum.photos/200" />
-      <main className=" px-3">
+      <FloatingButton />
+      <main className="bg-black px-3">
         {categories.map((category, index) => (
           <>
             <PreviewList
@@ -34,6 +34,6 @@ export default function Home() {
           </>
         ))}
       </main>
-    </>
+    </div>
   );
 }
