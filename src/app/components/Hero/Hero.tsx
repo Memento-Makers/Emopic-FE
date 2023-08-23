52cda04424838ce78966fb39f926a6d6e1666ffa
dar-jeeling <o_o___t@pusan.ac.kr>
2023-08-23 13:31:37 +0900

    ✨ feat: Hero 컴포넌트 구현

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,27 @@
+import BackgroundImage from '../../../../public/assets/soragrit-wongsa-aV5xrpB0bwQ-unsplash.jpg';
+
+export const Hero = () => {
+  return (
+    <div
+      className="hero min-h-screen bg-base-200"
+      style={{
+        backgroundImage: `url(/assets/soragrit-wongsa-aV5xrpB0bwQ-unsplash.jpg)`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold text-white">안녕하세요!</h1>
+          <p className="py-6 text-white">
+            당신의 추억과 감정을 사진에 담아보세요.
+            <br />
+            사진의 내용으로 사진을 검색해보세요.
+            <br />
+            사진 속 한 장면, 그 순간의 감동을 다시 느껴보세요.
+          </p>
+          <button className="btn btn-primary">지금 바로 시작해보세요!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -5,3 +5,4 @@ export * from './Logo';
 export * from './FloatingButton';
 export * from './PreviewList';
 export * from './Spacer';
+export * from './Hero';
