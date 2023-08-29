b89746ecb60a84564848476a2a73a82db3f19bc5
dar-jeeling <o_o___t@pusan.ac.kr>
2023-08-29 19:13:04 +0900

    🐛 fix: FloatingButton의 동작 수정

--- src/app/components/FloatingButton/FloatingButton.tsx
+++ src/app/components/FloatingButton/FloatingButton.tsx
@@ -1,12 +1,25 @@
-import Link from 'next/link';
+'use client';
+
 import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 export const FloatingButton = () => {
   return (
-    <button className="btn btn-primary absolute bottom-4 right-4 p-2 rounded-full z-10 w-[65px] h-[65px] ">
-      <Link href="/upload">
-        <AiOutlineCloudUpload className=" text-[48px]" />
-      </Link>
-    </button>
+    <>
+      <label
+        htmlFor="fileInput"
+        className="absolute bottom-4 right-4 p-2 rounded-full z-10 w-[65px] h-[65px] bg-primary cursor-pointer transition-all duration-300 ease-in-out shadow-md hover:scale-110 active:scale-100"
+      >
+        <div className="tooltip tooltip-bottom" data-tip="사진 업로드">
+          <AiOutlineCloudUpload className="text-white text-[48px] animate-pulse" />
+        </div>
+      </label>
+      <input
+        id="fileInput"
+        type="file"
+        className=" hidden"
+        multiple
+        accept="image/*"
+      />
+    </>
   );
 };
