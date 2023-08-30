39a885488357a417140510401159de5345268d1e
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-08-30 15:01:53 +0900

    ✨ feat: PhotoDetailHeader 컴포넌트 구현 (#44)

---
+++ src/app/components/ChatBubble/ChatBubble.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export interface ChatBubbleProps {
+  className?: string;
+  content: string;
+}
+
+export const ChatBubble = ({ className, content }: ChatBubbleProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 100);
+  }, []);
+
+  return (
+    <div
+      className={`transition-all duration-500 ease-in-out ${
+        isVisible
+          ? 'opacity-100 translate-y-0'
+          : 'opacity-0 translate-y-[-100px]'
+      } ${className}`}
+    >
+      <div className="chat chat-end rotate-[270deg]">
+        <div className="chat-bubble h-[300px]"></div>
+      </div>
+      <p className="absolute bottom-[44%] left-[-150%] w-[300px] text-white">
+        {content}
+      </p>
+    </div>
+  );
+};
