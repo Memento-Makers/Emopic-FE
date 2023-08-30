a6c6bb3a1fe6df90f770078d73911a9ab62e00d9
dar-jeeling <o_o___t@pusan.ac.kr>
2023-08-30 14:42:12 +0900

    ✨ feat: EmotionIcon 컴포넌트 구현

---
+++ src/app/components/EmotionIcon/EmotionIcon.tsx
@@ -0,0 +1,25 @@
+import { emotionData } from '@/components';
+import { EmotionId } from '@/types';
+
+export interface EmotionIconProps {
+  emotionId: EmotionId;
+}
+
+export const EmotionIcon = ({ emotionId }: EmotionIconProps) => {
+  const emotionClass = emotionId.toString()[0];
+
+  return (
+    <div
+      className={`flex flex-col items-center w-[120px] rounded-lg text-neutral p-[10px] box-border ${
+        emotionClass === '1'
+          ? 'bg-yellow-100'
+          : emotionClass === '2'
+          ? 'bg-green-200'
+          : 'bg-blue-200'
+      } bg-opacity-50`}
+    >
+      <div className={'text-[70px]'}>{emotionData[emotionId]?.icon}</div>
+      <p className="mt-2 text-[18px]">{emotionData[emotionId]?.label}</p>
+    </div>
+  );
+};
