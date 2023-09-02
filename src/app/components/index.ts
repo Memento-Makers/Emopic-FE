581e9209acd59772bfb1e2bd3abb508267250f6f
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-02 18:20:59 +0900

    🔥 remove: 사용하지 않는 컴포넌트 삭제

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -21,7 +21,6 @@ export * from './EmotionModalContent';
 export * from './FloatingButton';
 export * from './PhotoDetailHeader';
 export * from './AddEmotionButton';
-export * from './DiaryBottomSheet';
 export * from './ModalDialog';
 export * from './DetailBottomNavigation';
 export * from './DiaryPageHeader';
