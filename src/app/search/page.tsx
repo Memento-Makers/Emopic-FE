da253f594789de5569325adc5801bc5cc3b51c4c
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-05 04:25:30 +0900

    ✨ feat: search page 기능 구현

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,19 +5,31 @@ import {
   FloatingButton,
   BeforeUploadToastContent,
   AfterFileUploadToastContent,
+  PreviewList,
+  Spacer,
+  PreviewListSkeleton,
 } from '@/components';
-import { useMutation } from '@tanstack/react-query';
-import { fetchSignedURL, fetchCaption, fetchCategories } from '@/mocks';
 import { toast } from 'react-toastify';
-
+import {
+  useGetSignedURL,
+  useGetCaption,
+  useGetClassification,
+  uploadFile,
+  useCategoryAll,
+} from '@/api';
+import dayjs from 'dayjs';
+import { Fragment } from 'react';
 // TODO: 현재는 메인 페이지를 SearchPage 로 사용하고 있으나,
 // 이 후에 메인 페이지가 변화할 경우 플로팅 버튼과 관련된 로직을
 // 해당 페이지로 옮겨야 한다.
 
 export default function SearchPage() {
-  const uploadMutation = useMutation(fetchSignedURL);
-  const captionMutation = useMutation(fetchCaption);
-  const categoriesMutation = useMutation(fetchCategories);
+  const getSignedURLMutation = useGetSignedURL();
+  const getCaption = useGetCaption();
+  const getClassification = useGetClassification();
+
+  // TODO: 추후에 SSR 적용하기
+  const { data: categoryData, isLoading: isCategoryLoading } = useCategoryAll();
 
   const handleFileChange = async (files: File[]) => {
     if (files.length > 0) {
@@ -32,20 +44,33 @@ export default function SearchPage() {
       );
 
       try {
-        const uploadPromises = files.map(async file => {
+        const uploadPromises = files.map(async (file, index) => {
           // fetchSignedURL 실행
-          const {
-            data: { signedUrl, photoId },
-          } = await uploadMutation.mutateAsync(file.name);
+          const fileName =
+            dayjs().format('YYYYMMDDHHmmssSSS') +
+            process.env.NEXT_PUBLIC_DUMMY_USER_ID +
+            index;
+
+          const { photoId, signedUrl } = await getSignedURLMutation.mutateAsync(
+            fileName
+          );
+
+          console.log('signedUrl', signedUrl);
+          console.log('photoId', photoId);
 
           // 실제 파일 업로드 로직
-          // await uploadFile(signedUrl, file);
+          await uploadFile(signedUrl, file);
 
-          // fetchCaption과 fetch Categories 실행
-          const captionData = await captionMutation.mutateAsync(photoId);
-          const categoriesData = await categoriesMutation.mutateAsync(photoId);
+          // 이미지 캡션 요청
+          const { caption } = await getCaption.mutateAsync(photoId);
 
-          return { captionData, categoriesData, photoId };
+          // 이미지 분류 요청
+          const { categories } = await getClassification.mutateAsync(photoId);
+
+          console.log('caption', caption);
+          console.log('categories', categories);
+
+          return { caption, categories, photoId };
         });
 
         const results = await Promise.all(uploadPromises);
@@ -55,8 +80,10 @@ export default function SearchPage() {
         });
 
         // TODO: 확인용 콘솔, API 연결 후 지우기
-        console.log('업로드 완료', results);
+        console.log('업로드 완료');
       } catch (error) {
+        console.log('error', error);
+
         toast.error('파일을 업로드하는데 문제가 발생하였습니다.', {
           position: toast.POSITION.BOTTOM_RIGHT,
         });
@@ -65,26 +92,34 @@ export default function SearchPage() {
   };
 
   return (
-    <div className="relative">
+    <div className="relative h-[100vh] flex flex-col">
       <BasicHeader profileImage="https://picsum.photos/200" />
       <FloatingButton
         handleFileChange={files => {
           handleFileChange(files);
         }}
       />
-      <main className="px-3">
-        {/* {categories.map((category, index) => (
-            <>
+      <main className="px-3 flex-grow w-[100%]">
+        {isCategoryLoading &&
+          Array.from({ length: 6 }).map((_, index) => (
+            <PreviewListSkeleton key={`preview/skeleton/${index}`} />
+          ))}
+
+        {!isCategoryLoading &&
+          categoryData!.categories &&
+          categoryData!.categories.map(({ categoryId, name, count }, index) => (
+            <Fragment key={categoryId}>
               <PreviewList
-                key={index}
-                images={category.images}
-                title={category.title}
+                key={`${categoryId}/${name}`}
+                title={name}
+                categoryId={categoryId}
+                count={count}
               />
-              {index < categories.length - 1 && (
-                <Spacer size={24} key={index} />
+              {index < categoryData!.categories.length - 1 && (
+                <Spacer size={24} key={`${categoryId}/${name}/${index}`} />
               )}
-            </>
-          ))} */}
+            </Fragment>
+          ))}
       </main>
     </div>
   );
