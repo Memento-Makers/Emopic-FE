dfa15aa10404d255b296fbe27d880326989c6d85
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-05 04:19:37 +0900

    👽️ api: 명세에 맞게 리턴타입 수정

--- src/api/domain/category.ts
+++ src/api/domain/category.ts
@@ -1,5 +1,5 @@
 import { basicFetch, mutateFetch } from '../fetchFunctions';
-import { DetailedCategoryResponse, CategoryAllResponse } from '@/types';
+import { CategoryDetailData, CategoryAllData } from '@/types';
 
 export const categoryAPI = {
   // 이미지 분류 결과 요청 (분류 결과 조회)
@@ -10,18 +10,20 @@ export const categoryAPI = {
     return result.data;
   },
   // 분류 결과 세부 조회
-  getCategories: async (
-    categoryId: number
-  ): Promise<DetailedCategoryResponse> => {
-    const result = await basicFetch<DetailedCategoryResponse>(
-      `api/v1/photos/categories/${categoryId}`
+  getCategoryDetail: async (
+    categoryId: number,
+    size?: number
+  ): Promise<CategoryDetailData> => {
+    const result = await basicFetch<CategoryDetailData>(
+      `api/v1/photos/categories/${categoryId}?size=${size ? size : '10'}`
     );
+
     return result.data;
   },
   // 분류 결과 전체 조회
-  getCategoriesAll: async (size?: number): Promise<CategoryAllResponse> => {
-    const result = await basicFetch<CategoryAllResponse>(
-      `/api/v1/photos/catgories?size=${size ? size : ''}`
+  getCategoriesAll: async (size?: number) => {
+    const result = await basicFetch<CategoryAllData>(
+      `api/v1/photos/categories?size=${size ? size : '6'}`
     );
     return result.data;
   },
