65cd301c3d41d8ab69c696a3a4a1f816cc634330
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-05 04:23:56 +0900

    ✨ feat: PreviewListSkeleton 컴포넌트 구현

--- src/app/components/PreviewList/index.ts
+++ src/app/components/PreviewList/index.ts
@@ -1,3 +1,4 @@
-export * from './PreviewList';
+export { default as PreviewList } from './PreviewList';
 export * from './PreviewListItem';
 export * from './PreviewHeader';
+export { default as PreviewListSkeleton } from './PreviewListSkeleton';
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,7 +2,6 @@ export * from './BasicHeader';
 export * from './Avatar';
 export * from './StyledImage';
 export * from './Logo';
-export * from './PreviewList';
 export * from './Spacer';
 export * from './Hero';
 export * from './LinkButton';
@@ -15,6 +14,7 @@ export * from './DiaryContent';
 export * from './EmotionIcon';
 export * from './BeforeUploadToastContent';
 export * from './AfterFileUploadToastContent';
+export * from './Skeleton';
 
 // client component
 export * from './EmotionModalContent';
@@ -24,6 +24,7 @@ export * from './AddEmotionButton';
 export * from './ModalDialog';
 export * from './DetailBottomNavigation';
 export * from './DiaryPageHeader';
+export * from './PreviewList';
 
 // util component
 export * from './Provider';
