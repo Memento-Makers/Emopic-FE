d4653665ec89edbd5d8cb08562d17b0fef7925c6
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-06 01:35:38 +0900

    ✨ feat: 감정 선택 후 일기장 생성하는 기능 구현

--- src/app/components/DetailBottomNavigation/DetailBottomNavigation.tsx
+++ src/app/components/DetailBottomNavigation/DetailBottomNavigation.tsx
@@ -14,17 +14,22 @@ import { useRouter } from 'next/navigation';
 import { Emotion, EmotionId } from '@/types';
 import useSaveEmotion from '@/api/queries/domain/photo/hooks/useSaveEmotion';
 import { useEffect, useState } from 'react';
+import { EmotionDataMap } from '@/model';
+import { getOpenAIResponse } from '@/api';
+import { toast } from 'react-toastify';
 
 interface DetailBottomNavigationProps {
   photoId: number;
   photoUrl: string; // 사진 url
   emotionList: Emotion[]; // 선택되어 있는 감정 리스트
+  caption: string;
 }
 
 const DetailBottomNavigation = ({
   photoId,
   photoUrl,
   emotionList,
+  caption,
 }: DetailBottomNavigationProps) => {
   const router = useRouter();
 
@@ -71,6 +76,22 @@ const DetailBottomNavigation = ({
     photoEmotionIds.forEach(emotionId => newInitialMap.set(emotionId, true));
     setInitialMap(new Map(newInitialMap));
     setEmotionMap(new Map(newInitialMap)); // 초기 상태를 emotionMap에도 설정
+
+    toast('감정 입력 완료! 일기장 내용을 생성합니다.', {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+
+    // 일기장 작성하기
+    // 1. 감정을 string 으로 변환
+    const emotions = photoEmotionIds.map(id => EmotionDataMap.get(id));
+
+    // 2. openAI를 통해 뭐시기를 만든다.
+    const openAiRes = await getOpenAIResponse(emotions as string[], caption);
+    const diaryContent = openAiRes.choices[0].message.content;
+
+    // 3. 감정추가 API 보내고
+
+    // 4. 토스트도 하나 띄운다.
   };
 
   return (
--- src/app/components/DetailBottomNavigation/index.ts
+++ src/app/components/DetailBottomNavigation/index.ts
@@ -1,2 +1,3 @@
 export { default as DetailBottomNavigation } from './DetailBottomNavigation';
 export { default as NavigationButton } from './NavigationButton';
+export { default as DetailBottomNavigationSkeleton } from './DetailBottomNavigationSkeleton';
