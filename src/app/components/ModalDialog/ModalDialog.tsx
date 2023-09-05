2a121f1c48156b375419d84a8a3b95345c100606
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-06 00:24:34 +0900

    ✨ feat: ModalDialog가 closeCallback을 받도록 변경

--- src/app/components/ModalDialog/ModalDialog.tsx
+++ src/app/components/ModalDialog/ModalDialog.tsx
@@ -4,16 +4,21 @@ import React, { forwardRef, Ref } from 'react';
 
 interface ModalDialogProps {
   children: React.ReactNode;
+  closeCallback?: () => void;
 }
 
 const ModalDialog = forwardRef<HTMLDialogElement, ModalDialogProps>(
   (props, ref: Ref<HTMLDialogElement>) => {
-    const { children } = props;
+    const { children, closeCallback } = props;
 
     return (
       <dialog ref={ref} className="modal modal-bottom sm:modal-middle">
         <form method="dialog" className="modal-box relative">
           {children}
+        </form>
+
+        <form method="dialog" className="modal-backdrop">
+          <button onClick={closeCallback}>close</button>
         </form>
       </dialog>
     );
