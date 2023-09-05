f5cebbeb3115549b2ceb0ab4905b05ac98b6ab4d
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-05 20:19:07 +0900

    ✨ feat: PhotoDetailHeaderSkeleton 구현

---
+++ src/app/components/PhotoDetailHeader/PhotoDetailHeaderSkeleton.tsx
@@ -0,0 +1,29 @@
+import { PiCaretLeftLight } from 'react-icons/pi';
+import { useRouter } from 'next/navigation';
+
+const PhotoDetailHeaderSkeleton = () => {
+  const router = useRouter();
+
+  return (
+    <header className=" top-0 flex justify-center items-center p-3 w-[100%] relative">
+      <button
+        className="button text-[48px] text-primary absolute left-2"
+        onClick={() => router.back()}
+      >
+        <PiCaretLeftLight />
+      </button>
+
+      <div className=" flex justify-between items-center flex-col">
+        <ul className="flex items-center gap-2 mb-3">
+          <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-[50px]"></div>
+          <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-[50px]"></div>
+          <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-[50px]"></div>
+        </ul>
+
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+      </div>
+    </header>
+  );
+};
+
+export default PhotoDetailHeaderSkeleton;
