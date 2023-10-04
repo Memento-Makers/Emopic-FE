431c4d66cbcc7ccdae90101c4bcb0b086c44db40
dar-jeeling <o_o___t@pusan.ac.kr>
2023-10-04 18:38:50 +0900

    🔧 env: next.config에 hostname 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos', 'storage.googleapis.com/', 'img.freepik.com'],
+    domains: ['picsum.photos', 'storage.googleapis.com', 'img.freepik.com'],
   },
 };
 
