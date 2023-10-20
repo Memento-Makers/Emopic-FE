5cc1c1c028e668c0bad751027806f5e1b2569fc7
dar-jeeling <o_o___t@pusan.ac.kr>
2023-10-21 04:02:42 +0900

    🐛 fix: key에러 및 type 에러 수정

--- src/app/components/MainPhotoPreview/MainPhotoPreview.tsx
+++ src/app/components/MainPhotoPreview/MainPhotoPreview.tsx
@@ -1,6 +1,6 @@
 import { ThumbnailPhotoData } from '@/types';
 import Link from 'next/link';
-import { EmotionIcon, StyledImage } from '@/components';
+import { EmotionIconSmall, StyledImage } from '@/components';
 
 interface MainPhotoPreviewProps {
   date: string; // 문자열로 변환된 날짜 스트링
@@ -12,28 +12,28 @@ const MainPhotoPreview = ({ date, photos }: MainPhotoPreviewProps) => {
     <div className="mb-[24px]">
       <p className=" text-[18px] font-bold mb-[16px]">{date}</p>
       <ul className=" grid grid-cols-3 gap-[2px]">
-        {photos.map(photo => (
-          <Link href={`/photos/${photo.photoId}`}>
-            <div key={photo.photoId} className=" flex flex-col gap-[2px]">
-              <StyledImage
-                src={photo.thumbnailUrl}
-                alt={'썸네일 이미지'}
-                className={`rounded overflow-hidden`}
-                width={145}
-                height={145}
-              />
-              <div>
-                {/* <EmotionIcon emotionId={photo.emotions.main[0].emotionId} />
-                {photo.emotions.sub.slice(2).map(subEmotion => (
-                  <EmotionIcon
-                    key={`${photo.photoId}/emotion/${subEmotion.emotionId}`}
-                    emotionId={subEmotion.emotionId}
-                  />
-                ))} */}
-              </div>
-            </div>
-          </Link>
-        ))}
+        {photos.map(photo => {
+          return (
+            <Link key={photo.photoId} href={`/photos/${photo.photoId}`}>
+              <li className=" flex flex-col gap-[2px] relative">
+                <StyledImage
+                  src={photo.thumbnailUrl}
+                  alt={'썸네일 이미지'}
+                  className={`rounded overflow-hidden`}
+                  width={145}
+                  height={145}
+                />
+                <div className=" flex gap-[2px] absolute bottom-[2px] right-[4px]">
+                  {photo.emotions.main.length !== 0 && (
+                    <EmotionIconSmall
+                      emotionId={photo.emotions.main[0].emotionId}
+                    />
+                  )}
+                </div>
+              </li>
+            </Link>
+          );
+        })}
       </ul>
     </div>
   );
