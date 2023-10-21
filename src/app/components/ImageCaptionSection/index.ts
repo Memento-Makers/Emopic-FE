99771cb66ada7a6824c5f95f08ad5df49890e989
dar-jeeling <o_o___t@pusan.ac.kr>
2023-10-21 18:50:18 +0900

    🔥 remove: 사용하지 않는 파일 삭제

--- src/app/components/ImageCaptionSection/index.ts
+++
@@ -1,1 +0,0 @@
-export { ImageCaptionSection } from './ImageCaptionSection';
--- src/utils/test/index.ts
+++
@@ -1,8 +0,0 @@
-// 테스트에 사용되는 유틸 함수
-
-export const randomDelay = () => {
-  return new Promise(resolve => {
-    const delay = Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000; // 1~3초 사이의 랜덤 딜레이
-    setTimeout(resolve, delay);
-  });
-};
