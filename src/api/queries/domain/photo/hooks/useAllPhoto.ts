a44c5b58483c2d4d5f1ff94d5e78d0dcad7421f6
dar-jeeling <o_o___t@pusan.ac.kr>
2023-10-29 14:40:08 +0900

    ♻️ refactor: 고정 값으로 되어있는 userID 값 변경

--- src/api/queries/domain/photo/hooks/useAllPhoto.ts
+++ src/api/queries/domain/photo/hooks/useAllPhoto.ts
@@ -2,9 +2,11 @@ import { photoAPI, photoKeys } from '@/api';
 import { AllPhotoData } from '@/types';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
+const userId = parseInt(process.env.NEXT_PUBLIC_DUMMY_USER_ID as string) || 1;
+
 const useAllPhoto = () =>
   useInfiniteQuery<AllPhotoData>({
-    queryKey: photoKeys.all(1),
+    queryKey: photoKeys.all(userId),
     queryFn: async ({ pageParam = 0 }) => {
       const res = await photoAPI.getAll(pageParam);
       return res;
