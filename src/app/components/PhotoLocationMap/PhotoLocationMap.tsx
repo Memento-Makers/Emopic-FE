d6282b0aca3008f3771597a18b49a5580106f9d3
dar-jeeling <o_o___t@pusan.ac.kr>
2023-10-29 14:02:36 +0900

    ✨ feat: 지도에서 사진 클릭 시 개별 사진 페이지로 가게 수정

--- src/app/components/PhotoLocationMap/PhotoLocationMap.tsx
+++ src/app/components/PhotoLocationMap/PhotoLocationMap.tsx
@@ -2,7 +2,7 @@
 
 import { CustomOverlayMap, Map, MarkerClusterer } from 'react-kakao-maps-sdk';
 import { PhotoWithLocation } from '@/types';
-
+import Link from 'next/link';
 export interface PhotoLocationMapProps {
   photos: PhotoWithLocation[];
 }
@@ -27,15 +27,21 @@ const PhotoLocationMap = ({ photos }: PhotoLocationMapProps) => {
                   lng: photo.longitude,
                 }}
               >
-                <div className=" w-[100px] h-[100px] border-[3px] border-white rounded-lg shadow-md overflow-hidden">
-                  <img
-                    src={photo.thumbnailUrl}
-                    alt={'썸네일 이미지'}
-                    width={100}
-                    height={100}
-                    className="rounded-lg"
-                  />
-                </div>
+                <Link
+                  href={`photos/${photo.photoId}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className=" w-[100px] h-[100px] border-[3px] border-white rounded-lg shadow-md overflow-hidden">
+                    <img
+                      src={photo.thumbnailUrl}
+                      alt={'썸네일 이미지'}
+                      width={100}
+                      height={100}
+                      className="rounded-lg"
+                    />
+                  </div>
+                </Link>
               </CustomOverlayMap>
             </div>
           );
